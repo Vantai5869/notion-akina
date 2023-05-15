28b73519002c576fb4ba4f734f9248cdd70b3c41
sucwin <phamvantai2202@gmail.com>
2023-05-15 23:40:51 +0700

    build

---
+++ client/build/precache-manifest.e627d1c18086b83c5befe24b44d36a58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c72ee86c27bf1ec1eb4692f8a40c7ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf7d4a5e0b70ae48830f",
+    "url": "/static/js/2.3a94abd0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3a94abd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb95eb5bf101c38c5849",
+    "url": "/static/js/main.a543915f.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
